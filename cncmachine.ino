d83924bbb2454ef4658504436912571aee2dbaaa
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-01-05 10:21:53 -0500

    Update cncmachine.ino
    Good point of the CNC machine code: send commands through serial without newline enabled such as M1:10000,M2:10000,M3:-1000,S:3000; and what that would do is  tell the 1st motor (2 rails) to move 10000 steps, the second motor (the rail being moved by the 2 stationary ones) to move 10000 steps, and the 3rd motor(the drilling bit motor) to move 1000 steps up. The S:3000; makes it take 3000 milliseconds aka 3 seconds to move the motor.

--- cncmachine.ino
+++ cncmachine.ino
@@ -1,10 +1,13 @@
 #include "StepperController.h"
 
-StepperController sMotor1(33,25,22,true,10000);
+StepperController sMotor1(33,25,22,true,200000);
 StepperController sMotor2(26,27,1,true,10000);
 StepperController sMotor3(14,12,23,true,10000);
 
-int motorSpeed = 3000;
+int motorTime = 1000;
+int sMotor1Target = 0;
+int sMotor2Target = 0;
+int sMotor3Target = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -17,19 +20,35 @@ void readSerial() {
   if (Serial.available() > 0) {
     String str = Serial.readString();
     if(str.indexOf("M1") >= 0){
-      sMotor1.setTarget(str.substring(3,str.indexOf(";")).toInt(),motorSpeed);
-    } else if(str.indexOf("M2") >= 0){
-      sMotor1.setTarget(str.substring(3,str.indexOf(";")).toInt(),motorSpeed);
-    } else if(str.indexOf("M3") >= 0){
-      sMotor1.setTarget(str.substring(3,str.indexOf(";")).toInt(),motorSpeed);
+      sMotor1Target = str.substring(str.indexOf("M1") + 3,str.indexOf("M2")).toInt();
+    }
+    if(str.indexOf("M2") >= 0){
+      sMotor2Target = str.substring(str.indexOf("M2") + 3,str.indexOf("M3")).toInt();
+    }
+    if(str.indexOf("M3") >= 0){
+      sMotor3Target = str.substring(str.indexOf("M3") + 3,str.indexOf("S")).toInt();
     } else if(str.indexOf("S") >= 0){
-      motorSpeed = str.substring(2,str.indexOf(";")).toInt();
+      motorTime = str.substring(str.indexOf("S") + 2,str.indexOf(";")).toInt();
     }
   }
 }
 
 void loop() {
   readSerial();
+  if(millis() % 2000 == 0) {
+    if(sMotor1Target != 0){
+      sMotor1.setTarget(sMotor1Target,abs(sMotor1Target) / (int)((double)motorTime / 1000.0));
+      sMotor1Target = 0;
+    }
+    if(sMotor2Target != 0){
+      sMotor2.setTarget(sMotor2Target,abs(sMotor2Target) / (int)((double)motorTime / 1000.0));
+      sMotor2Target = 0;
+    }
+    if(sMotor3Target != 0){
+      sMotor3.setTarget(sMotor3Target,abs(sMotor3Target) / (int)((double)motorTime / 1000.0));
+      sMotor3Target = 0;
+    }
+  }
   sMotor1.process();
   sMotor2.process();
   sMotor3.process();
